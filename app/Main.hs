8c051336e5b667641e7afada29a4ebc861e364a5
Le1ne <themegabrain8@gmail.com>
2024-04-16 03:37:34 +0300

    added func bodies and instances

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,57 @@
 module Main (main) where
 
-import CashReceipt
+import CashReceipt (Product, CartItem, BonusCard, Products, Cart)
 import System.IO
 import Control.Monad
 import Data.Maybe
+import System.Directory (doesFileExist)
 
 parseProduct :: String -> Product
+parseProduct str = let [name, priceStr, category] = wordsWhen (==',') str
+                   in Product name (read priceStr) category
 
 parseCartItem :: String -> CartItem
+parseCartItem str = let [name, quantityStr] = wordsWhen (==',') str
+                    in CartItem name (read quantityStr)
 
 parseBonusCard :: String -> BonusCard
+parseBonusCard str = let [birthdayStr, discountRateStr] = wordsWhen (==',') str
+                     in BonusCard (if null birthdayStr then Nothing else Just birthdayStr) (read discountRateStr)
 
 wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s = case dropWhile p s of
+                  "" -> []
+                  s' -> w : wordsWhen p s''
+                        where (w, s'') = break p s'
 
-eadProducts :: FilePath -> IO Products
+readProducts :: FilePath -> IO Products
+readProducts filePath = do
+	contents <- readFile filePath
+	return $ map parseProduct (lines contents)
 
 readCart :: FilePath -> IO Cart
+readCart filePath = do
+	contents <- readFile filePath
+	return $ map parseCartItem (lines contents)
 
 readBonusCard :: FilePath -> IO (Maybe BonusCard)
+readBonusCard filePath = do
+  	exists <- doesFileExist filePath
+  	if exists
+  	then fmap (Just . parseBonusCard) (readFile filePath)
+  	else return Nothing
 
 processFiles :: FilePath -> FilePath -> FilePath -> IO ()
+processFiles productsPath cartPath bonusCardPath = do
+	products <- readProducts productsPath
+  	cart <- readCart cartPath
+  	bonusCardOpt <- readBonusCard bonusCardPath
+  	let bonusCard = fromMaybe (BonusCard Nothing 0) bonusCardOpt
+  	printReceipt cart products bonusCard
 
 main :: IO ()
-main = someFunc
+main = do
+  	let productsPath = "products.txt"
+  	let cartPath = "cart.txt"
+  	let bonusCardPath = "bonus_card.txt"
+  	processFiles productsPath cartPath bonusCardPath
--- src/CashReceipt.hs
+++ src/CashReceipt.hs
@@ -1,16 +1,67 @@
 module CashReceipt
-    ( findProductPrice
-      calculateItemCost
-      calculateTotal
-      calculateDiscount
+    ( Product,
+	  CartItem, 
+	  BonusCard,
+	  Products,
+	  Cart,
+	  findProductPrice,
+      calculateItemCost,
+      calculateTotal,
+      calculateDiscount,
       calculateFinalTotal
     ) where
 
-type Name = String
-type Price = Double
-type Category = String
-type Quantity = Int
-type Discount = Double
+import Data.List (lookup)
+
+newtype Name = Name String
+newtype Price = Price Double
+newtype Category = Category String
+newtype Quantity = Quantity Int
+newtype Discount = Discount Double
+
+instance Show Price where
+    show (Price price) = show price
+
+instance Show Quantity where
+    show (Quantity quantity) = show quantity
+
+instance Show Name where
+    show (Name name) = show name
+
+instance Show Discount where
+    show (Discount discount) = show discount
+
+instance Show Category where
+    show (Category category) = show category
+
+instance Eq Name where
+    (Name name1) == (Name name2) = name1 == name2
+
+instance Enum Quantity where
+    toEnum n = Quantity (toEnum n)
+    fromEnum (Quantity q) = fromEnum q
+
+instance Eq Quantity where
+    (Quantity a) == (Quantity b) = a == b
+    (Quantity a) /= (Quantity b) = a /= b
+
+instance Ord Quantity where
+    compare (Quantity a) (Quantity b) = compare a b
+
+instance Num Quantity where
+    (Quantity a) + (Quantity b) = Quantity (a + b)
+    (Quantity a) * (Quantity b) = Quantity (a * b)
+    abs (Quantity a) = Quantity (abs a)
+    signum (Quantity a) = Quantity (signum a)
+    fromInteger n = Quantity (fromInteger n)
+    (Quantity a) - (Quantity b) = Quantity (a - b)
+
+instance Real Quantity where
+    toRational (Quantity q) = toRational q
+
+instance Integral Quantity where
+    toInteger (Quantity q) = toInteger q
+    quotRem (Quantity a) (Quantity b) = let (q, r) = quotRem a b in (Quantity q, Quantity r)
 
 data Product = Product Name Price Category deriving (Show)
 data CartItem = CartItem Name Quantity deriving (Show)
@@ -19,12 +70,23 @@ data BonusCard = BonusCard { birthday :: Maybe String, discountRate :: Discount 
 type Products = [Product]
 type Cart = [CartItem]
 
+getPrice :: Price -> Double
+getPrice (Price p) = p
+
+getDiscount :: Discount -> Double
+getDiscount (Discount d) = d
+
 findProductPrice :: Name -> Products -> Maybe Price
+findProductPrice name products = lookup name [(productName, price) | Product productName price _ <- products]
 
 calculateItemCost :: CartItem -> Products -> Maybe Double
+calculateItemCost (CartItem name quantity) products = fmap ((* fromIntegral quantity) . getPrice) (findProductPrice name products)
 
 calculateTotal :: Cart -> Products -> Maybe Double
+calculateTotal cart products = sum <$> sequence (map (`calculateItemCost` products) cart)
 
 calculateDiscount :: Double -> BonusCard -> Double
+calculateDiscount total (BonusCard _ discountRate) = total * getDiscount(discountRate) / 100
 
 calculateFinalTotal :: Double -> Double -> Double
+calculateFinalTotal total discount = total - discount
